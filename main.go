398b1eb21c51466f88e14e76fe8d8400d4b96d4f
Sahil Naikdhure <sanaikdhure@gmail.com>
2021-05-13 11:06:08 +0530

    add comments and gitignore file

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// card := "Ace of Spades" // := is used to a declare and initialise a new variable.
+	card := newCard()
+
+	fmt.Println(card)
+}
+
+func newCard() string { // since we return a string value from this function.
+	return "Five of Diamonds"
 }
